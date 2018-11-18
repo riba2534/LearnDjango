3edb37ecaa13e1bd199aff19e0f4ec7ffb7031d5
riba2534 <riba2534@qq.com>
2018-11-18 20:41:45 +0800

    改变blog目录

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -5,13 +5,13 @@ from .models import *
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
-    return render_to_response("blog_list.html", context)
+    return render_to_response("blog/blog_list.html", context)
 
 
 def blog_detail(request, blog_pk):  # 博客内容
     context = {}
     context['blog'] = get_object_or_404(Blog, id=blog_pk)
-    return render_to_response("blog_detail.html", context)
+    return render_to_response("blog/blog_detail.html", context)
 
 
 def blogs_with_type(request, blog_type_pk):
@@ -19,4 +19,4 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, id=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
-    return render_to_response("blogs_with_type.html", context)
+    return render_to_response("blog/blogs_with_type.html", context)
