90158387552e3b91693a5faadec7e7b0cac75a73
riba2534 <riba2534@qq.com>
2018-11-30 20:37:51 +0800

    15 51:32

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -30,6 +30,8 @@ def blog_list(request):
     context['page_of_blogs'] = page_of_blogs
     context['page_range'] = page_range
     context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates(
+        'created_time', 'month', order='DESC')
     return render_to_response("blog/blog_list.html", context)
 
 
@@ -76,4 +78,32 @@ def blogs_with_type(request, blog_type_pk):
 
 
 def blogs_with_date(request, year, month):
-    pass
+    blogs_all_list = Blog.objects.filter(
+        created_time__year=year, created_time__month=month)
+    paginator = Paginator(
+        blogs_all_list, settings.EACH_PAGE_BLOGS_NUMBER)  # 每10页进行分页
+    page_num = request.GET.get('page', 1)  # get获取page的参数
+    page_of_blogs = paginator.get_page(page_num)  # 防止出错，防止用户瞎输
+    current_page_num = page_of_blogs.number  # 获得当前页码
+    # 当前页码前后两页
+    page_range = list(range(max(current_page_num-2, 1), current_page_num)) + \
+        list(range(current_page_num, min(current_page_num+2, paginator.num_pages)+1))
+    # 加省略号
+    if page_range[0] - 1 >= 2:
+        page_range.insert(0, '...')
+    if paginator.num_pages-page_range[-1] >= 2:
+        page_range.append('...')
+    # 加首尾页
+    if page_range[0] != 1:
+        page_range.insert(0, 1)
+    if page_range[-1] != paginator.num_pages:
+        page_range.append(paginator.num_pages)
+
+    context = {}
+    context['blogs'] = page_of_blogs.object_list
+    context['page_of_blogs'] = page_of_blogs
+    context['page_range'] = page_range
+    context['blog_types'] = BlogType.objects.all()
+    context['blog_dates'] = Blog.objects.dates(
+        'created_time', 'month', order='DESC')
+    return render_to_response("blog/blogs_with_type.html", context)
