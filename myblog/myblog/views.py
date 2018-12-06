1416f919686f16a84028e043d4b8b367dc2eba3f
riba2534 <riba2534@qq.com>
2018-12-06 15:18:30 +0800

    24结束，初步进行了使用Django-Form进行表单操作,实现了注册功能

--- myblog/myblog/views.py
+++ myblog/myblog/views.py
@@ -8,7 +8,8 @@ from django.db.models import Sum
 from django.core.cache import cache
 from django.contrib import auth
 from django.urls import reverse
-from .forms import LoginForm
+from .forms import LoginForm, RegForm
+from django.contrib.auth.models import User
 
 
 def get_7_days_hot_blogs():
@@ -42,17 +43,37 @@ def home(request):
 
 
 def login(request):
-    # username = request.POST.get('username', '')
-    # password = request.POST.get('password', '')
-    # user = auth.authenticate(request, username=username, password=password)
-    # referer = request.META.get(
-    #     'HTTP_REFERER', reverse('home'))  # reverse可以反向解析
-    # if user is not None:
-    #     auth.login(request, user)
-    #     return redirect(referer)
-    # else:
-    #     return render(request, 'error.html', {'message': '用户名或密码不正确', 'redircet_to': referer})
-    login_form = LoginForm()
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():  # 如果表单提交没问题,当前已经验证过用户名和密码
+            user = login_form.cleaned_data['user']
+            auth.login(request, user)
+            # 用get方式返回原来路径，否则跳转到首页
+            return redirect(request.GET.get('from'), reverse('home'))
+    else:
+        login_form = LoginForm()
     context = {}
     context['login_form'] = login_form
     return render(request, 'login.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        reg_form = RegForm(request.POST)
+        if reg_form.is_valid():
+            username = reg_form.cleaned_data['username']
+            password = reg_form.cleaned_data['password']
+            email = reg_form.cleaned_data['email']
+            # 创建用户
+            user = User.objects.create_user(username=username,
+                                            email=email, password=password)
+            user.save()
+            # 登录用户
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect(request.GET.get('from'), reverse('home'))
+    else:
+        reg_form = RegForm()
+    context = {}
+    context['reg_form'] = reg_form
+    return render(request, 'register.html', context)
